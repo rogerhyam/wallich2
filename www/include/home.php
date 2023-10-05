7d3a094bf4f17a729db58977a6728debf0dbcd27
Roger Hyam <rhyam@rbge.org.uk>
2023-10-05 17:23:05 +0100

    warning

--- www/include/home.php
+++ www/include/home.php
@@ -16,6 +16,16 @@
     <div class="field field-name-body field-type-text-with-summary field-label-hidden">
         <div class="field-items">
             <div class="field-item even" property="content:encoded">
+
+                <p style="border: solid 2px orange; padding: 1em; clear: both;">
+                    We are making some changes as we move the <em>Wallich Catalogue Online</em> to a new hosting
+                    platform.
+                    All the data is still here but it will take us a few more days to stitch it back together like
+                    before.
+                    Please carry on using the catalogue as usual.
+                    Work should be finished by the end of October 2023 after which we will look at adding integration
+                    with other databases.
+                </p>
                 <p>
                 <div class="media media-element-container media-default media-float-right">
                     <div id="file-26068" class="file file-image file-image-jpeg">
@@ -35,14 +45,18 @@
 
 
                     </div>
-                </div>The <em>Wallich Catalogue Online</em> is an interactive tool to help people use
-                the <em>Wallich Catalogue* </em>and interpret the herbarium specimens Nathaniel Wallich
-                distributed on behalf of the British East India Company between 1829 and 1847. The 306
-                lithographically printed pages list 9,148 &#39;Entries&#39;, usually species, and give
-                collecting details of 20,897 &#39;Collections&#39;. The pages were originally intended
-                to be cut up and used as specimen labels and, as the specimens are the basis of
-                thousands of plant names described from S and SE Asia, understanding these is important
-                in the typification and application of thousands of plant names in many Asian countries.
+                </div>
+
+
+                <p>
+                    The <em>Wallich Catalogue Online</em> is an interactive tool to help people use
+                    the <em>Wallich Catalogue* </em>and interpret the herbarium specimens Nathaniel Wallich
+                    distributed on behalf of the British East India Company between 1829 and 1847. The 306
+                    lithographically printed pages list 9,148 &#39;Entries&#39;, usually species, and give
+                    collecting details of 20,897 &#39;Collections&#39;. The pages were originally intended
+                    to be cut up and used as specimen labels and, as the specimens are the basis of
+                    thousands of plant names described from S and SE Asia, understanding these is important
+                    in the typification and application of thousands of plant names in many Asian countries.
                 </p>
                 <p>The numerous corrections and additions which occur sporadically through the
                     <em>Catalogue </em>are brought together and viewed under each Entry number.
